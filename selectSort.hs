c58bedb58c345586dc8cdb29aeca535a84fb467e
Denys <DenysBez@users.noreply.github.com>
2015-02-12 12:02:34 +0800

    Updated Selection Sort algorithm

--- selectSort.hs
+++ selectSort.hs
@@ -2,14 +2,14 @@
 Selection sort steps:
 Find the smallest item in the array, and exchange it with the first element. 
 Find the next smallest item and exchange it with the second entry.
+
+Complexity: todo
 --}
 selectSort :: (Ord a) => [a] -> [a]
 selectSort [] = []
 selectSort ilist = let
-    minElem = minimum ilist in minElem : selectSort (swapElem minElem ilist)
-    where swapElem _ [] = []
-          swapElem minElem (x:xs)
-            | minElem == x = xs
-            | otherwise = x : swapElem minElem xs
-
-
+    minElem = minimum ilist in minElem : selectSort (drop 1 (chngHeadPos minElem ilist))
+    where chngHeadPos _ [] = []
+          chngHeadPos minElem (x:xs)
+            | minElem == x = head ilist : xs
+            | otherwise = x : chngHeadPos minElem xs
